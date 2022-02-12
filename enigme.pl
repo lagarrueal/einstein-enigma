4fc9c34be4398e76047cbcc28b618845d49955cb
Alexandre <86953370+lagarrueal@users.noreply.github.com>
2022-02-12 22:49:06 +0100

    Translated code from french to english

--- enigme.pl
+++ enigme.pl
@@ -1,36 +1,36 @@
 :- use_module(library(clpfd)).
 createListe(L,NbLst,NbElt):-
-    length(L,NbLst), createListe(L,NbElt).
-createListe([],_).
-createListe([L1|R],NbElt) :-
-    length(L1,NbElt), createListe(R,NbElt).
+    length(L,NbLst), createList(L,NbElt).
+createList([],_).
+createList([L1|R],NbElt) :-
+    length(L1,NbElt), createList(R,NbElt).
 
-%ordre [couleur , nationalite , animal , boisson , cigarette]
+%ordre [color , nationality , animal , drink , cigarette]
 
-enigme(L) :-
-    createListe(L,5,5),
-    member([_,_,_,eau,_],L),
-    member([_,_,zebre,_,_],L),
-    nth1(1,L,[_,norvegien,_,_,_]),
-    nth1(2,L,[bleue,_,_,_,_]),
-    nth1(3,L,[_,_,_,lait,_]),
-    member([rouge,anglais,_,_,_],L),
-    member([verte,_,_,café,_],L),
-    member([jaune,_,_,_,kool],L),
-    nth1(Iverte,L,[verte,_,_,_,_]),
-    nth1(Iblanche,L,[blanche,_,_,_,_]),
-    Iblanche #= Iverte+1,
-    member([_,espagnol,chien,_,_],L),
-    member([_,ukrainien,_,the,_],L),
-    member([_,japonais,_,_,craven],L),
-    member([_,_,escargot,_,oldgold],L),
-    member([_,_,_,vin,gitane],L),
+enigma(L) :-
+    createList(L,5,5),
+    member([_,_,_,water,_],L),
+    member([_,_,zebra,_,_],L),
+    nth1(1,L,[_,norwegian,_,_,_]),
+    nth1(2,L,[blue,_,_,_,_]),
+    nth1(3,L,[_,_,_,milk,_]),
+    member([red,english,_,_,_],L),
+    member([green,_,_,coffee,_],L),
+    member([yellow,_,_,_,kool],L),
+    nth1(Igreen,L,[green,_,_,_,_]),
+    nth1(Iwhite,L,[white,_,_,_,_]),
+    Iwhite #= Igreen+1,
+    member([_,spanish,dog,_,_],L),
+    member([_,ukrainian,_,tea,_],L),
+    member([_,japanese,_,_,craven],L),
+    member([_,_,snail,_,oldgold],L),
+    member([_,_,_,wine,gitane],L),
     nth1(Ichester,L,[_,_,_,_,chesterfield]),
-    nth1(Irenard,L,[_,_,renard,_,_]),
-    abs(Ichester - Irenard) #= 1,
+    nth1(Ifox,L,[_,_,fox,_,_]),
+    abs(Ichester - Ifox) #= 1,
     nth1(Ikool,L,[_,_,_,_,kool]),
-    nth1(Icheval,L,[_,_,cheval,_,_]),
-    abs(Icheval-Ikool) #= 1.
+    nth1(Ihorse,L,[_,_,horse,_,_]),
+    abs(Ihorse-Ikool) #= 1.
 
 sup0(X) :-
     X >0.
